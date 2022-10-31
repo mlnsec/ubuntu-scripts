a21a16ef182262ab610e7f8dbbd53a2dc85770f5
Lucas Moulin <87871366+mlnsec@users.noreply.github.com>
2022-11-01 00:31:07 +0100

    Quick cleanup for extra apps

--- ubuntu-no-snaps.sh
+++ ubuntu-no-snaps.sh
@@ -2,7 +2,7 @@
 # Pretty ugly script to quickly configure
 # Ubuntu 22.04 LTS
 
-echo -e "\e[32;1mUninstalling all snaps...\e[m\n"
+echo -e "\e[32;1mSay goodbye to all snaps...\e[m\n"
 
 sudo pkill snap-store
 sudo systemctl disable snapd.service
@@ -21,7 +21,7 @@ sudo apt autoremove --purge snapd -y
 # Block snapd from reinstalling
 sudo apt-mark hold snapd
 
-echo -e "\e[32;1mInstalling Firefox...\e[m\n"
+echo -e "\e[32;1mInstalling Firefox from the mozillateam ppa...\e[m\n"
 # Add Firefox ppa
 sudo tee -a /etc/apt/preferences.d/firefox-no-snap &>/dev/null << 'EOF'
 Package: firefox*
@@ -32,10 +32,10 @@ EOF
 sudo add-apt-repository -y ppa:mozillateam/ppa
 sudo apt install -y firefox firefox-locale-fr
 
-# Install all the useful applications
-# For every computer I use
-echo -e "\e[32;1mAll the other applications...\e[m\n"
-# Libreoffice French
+# Install all the useful applications for every computer I use
+echo -e "\e[32;1mInstalling all the other applications...\e[m\n"
+
+# Libreoffice + French-specific packages
 sudo apt install -y libreoffice-style-yaru libreoffice-writer libreoffice-calc libreoffice-gnome libreoffice-impress libreoffice-l10n-fr hyphen-fr mythes-fr hunspell-fr-classical
 
 # Useful tools for GNOME
@@ -46,7 +46,7 @@ sudo apt install -y ffmpeg ffmpegthumbnailer emacs vim vlc yt-dlp deja-dup git c
 
 # Add "virtualbox steam shotwell" for Desktop computer
 
-# No use for mpv
+# I have no use for mpv which is pulled with yt-dlp
 sudo apt autoremove --purge -y mpv
 
 # For desktop and laptop
@@ -55,9 +55,13 @@ sudo apt autoremove --purge -y mpv
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo gpg --dearmor -o /usr/share/keyrings/spotify-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/spotify-archive-keyring.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
-# Proprietary Apps
-mkdir extradebs
-cd extradebs
+# Extra applications
+# I like everything to be a deb package
+
+mkdir extradebs && cd extradebs
+# To be improved to get the latest release...
 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.0.3/obsidian_1.0.3_amd64.deb
 wget https://mullvad.net/download/app/deb/latest/ -O mullvad.deb
 sudo apt install -y ./*.deb
+
+echo -e "\e[32;1mAll done! Hopefully :)\e[m\n"
